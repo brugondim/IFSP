41f84de95fea2cdd5e4da0c55a339b5400d3dd5d
brugondim <90776432+brugondim@users.noreply.github.com>
2021-10-20 10:50:20 -0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+const masks = {
+    CPF (value) {
+      return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+    },
+  
+    TELEFONE (value) {
+      return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1')
+    },
+   
+    CEP (value) {
+      return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    },
+  
+  
+    DATA DE NASCIMENTO (value) {
+      return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\/\d{2})(\d)/, '$1/$2')
+        .replace(/(\/\d{4})\d+?$/, '$1')
+    },
+  }
+  
+  document.querySelectorAll('input').forEach($input => {
+    const field = $input.dataset.js
+  
+    $input.addEventListener('input', e => {
+      e.target.value = masks[field](e.target.value)
+    }, false)
+  })
